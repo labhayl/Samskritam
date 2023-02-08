e76163321f1e6a1a9fdcec43be10a74800cd6653
Abhay Pratap Singh <60417905+labhayl@users.noreply.github.com>
2023-02-08 09:52:39 +0530

    Update vowel_diacritic_extractor.py

--- 2023feb_conf/vowel_diacritic_extractor.py
+++ 2023feb_conf/vowel_diacritic_extractor.py
@@ -30,4 +30,12 @@ def get_vowels_diacritics(word):
     
     
 word = "राम"
-get_vowels_diacritics(word.ljust(8,' ')) # reducing whitespaces results in string out of bounds error with complex words , ex - रुद्र
+first_vowel,last_vowel=get_vowels_diacritics(word.ljust(8,' ')) # reducing whitespaces results in string out of bounds error with complex words , ex - रुद्र
+
+#first_vowel,last_vowel in function can be a diacritic as well,so we are assigning them corresponding vowel value in order to get corresponding sandhi
+first_vowel,last_vowel=' '+first_vowel,' '+last_vowel
+if first_vowel in diacritics:
+    first_vowel=vowels[diacritics.index(first_vowel)]
+if last_vowel in diacritics:
+    last_vowel=vowels[diacritics.index(last_vowel)]
+print(first_vowel,last_vowel)
