ba0d22b31ca7aaacf28cffab87cd16f9c2a9beff
sud0x00 <sudhanva0asvl@outlook.com>
2023-02-09 19:04:12 +0530

    new file:   2023feb_conf/Code/.streamlit/config.toml 	new file:   2023feb_conf/Code/Home.py 	new file:   2023feb_conf/Code/pages/Contact.py 	new file:   2023feb_conf/Code/pages/FAQ.py 	new file:   2023feb_conf/Papers/1801.00428.pdf 	new file:   2023feb_conf/Papers/IJCRTICGT064.pdf 	new file:   "2023feb_conf/sandhi-vibh\304\201janam_pipeline.pdf"

---
+++ 2023feb_conf/Code/Home.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import subprocess
+
+def run_script(input_text):
+    result = subprocess.run(["python", "your_script.py", input_text], stdout=subprocess.PIPE)
+    return result.stdout.decode("utf-8")
+
+title_page = """
+            <h1 style='text-align: center;'>कैलासः / kailāsaḥ </h1>
+            """
+st.markdown(title_page, unsafe_allow_html=True) 
+
+
+
+st.subheader("संस्कृतम् व्याकरणम्")
+st.subheader("saṃskṛtam vyākaraṇam ")
+input_text = st.text_input("Enter input text / कृपया शब्दान् लिखन्तु : ")
+if input_text:
+    output = run_script(input_text)
+    st.success("Output:")
+    st.text(output)
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
---
+++ 2023feb_conf/Code/pages/FAQ.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.header("Frequently Asked Questions")
+st.write(
+    """
+1. What is कैलासः / kailāsaḥ ?
+Ans-San: कैलासः तन्त्रांशसाधन शब्दं विभज्य सन्धिनाम लिखति। 
+Ans-Eng: Kailāsaḥ is a website that separates a word into its constituent parts and determines the sandhi type based on the combination of vowels in the split words. It provides the sandhi name and the separated words.
+"""
+)
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
